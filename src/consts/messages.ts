03f4648f3f6f50d8171961ae8d7f4882960a9d5c
Paweł Trela <pawel.trela@fingoweb.com>
2024-03-26 07:38:34 +0100

    fix linter errors

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -4,8 +4,11 @@ const ERROR_PROMOTED_PRODUCTS_MSG = 'ERROR_PROMOTED_PRODUCTS_MSG';
 const messagesMap = {
   [PRODUCTS_CONTAINER_NOT_FOUND]: 'Products container not found',
   [ERROR_PROMOTED_PRODUCTS_MSG]:
-  'An error occured while fetching promoted products',
-  
+    'An error occured while fetching promoted products',
 };
 
-export { messagesMap, PRODUCTS_CONTAINER_NOT_FOUND, ERROR_PROMOTED_PRODUCTS_MSG };
+export {
+  messagesMap,
+  PRODUCTS_CONTAINER_NOT_FOUND,
+  ERROR_PROMOTED_PRODUCTS_MSG,
+};
--- src/managers/AdManager/AdManager.ts
+++ src/managers/AdManager/AdManager.ts
@@ -1,11 +1,9 @@
 import { TPages } from 'types/pages';
-import { TAdProduct } from 'types/product';
-import getMessage from 'utils/getMessage';
 
 class AdManager {
   private page: TPages | null;
   private productsIds: number[];
 
-  constructor(page: TPages | null) {}
+  constructor() {}
 }
 export default AdManager;
