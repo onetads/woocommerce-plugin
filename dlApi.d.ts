3c423aad3c39557088423b85d58a24dbb5a7d58b
Paweł Trela <pawel.trela@fingoweb.com>
2024-03-27 07:55:27 +0100

    Feat: deleted useless stuff from dlapi types declaration files

--- dlApi.d.ts
+++ dlApi.d.ts
@@ -2,11 +2,6 @@ import { TFetchNativeAdOptions, TFetchNativeAdResponse } from 'types/dlApi';
 
 declare global {
   namespace dlApi {
-    let area: string;
-    let keyvalues: {
-      offer_ids: string;
-    };
-
     let cmd: {
       push: (fn: (dlApiObj: typeof dlApi) => void) => void;
     };
